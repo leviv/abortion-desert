1a56b5c45584ea7e34e5c2b170c8971b8fa41319
Levi Villarreal <villarreallevi@gmail.com>
2025-07-25 16:52:38 -0400

    add map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import us from '$lib/us.json';
+	import clinics from '$lib/a-clinics.json';
+	import * as d3 from 'd3';
+	import { geoAlbersUsa } from 'd3-geo';
+	import { feature } from 'topojson-client';
+	let el: HTMLDivElement;
+
+	onMount(() => {
+		// Set up the svg container
+		const chart = d3
+			.select(el)
+			.append('svg')
+			.attr('preserveAspectRatio', 'xMinYMin meet')
+			.attr('viewBox', '0 0 960 600')
+			.attr('stroke', '#fff')
+			.attr('fill', '#264653')
+			.classed('svg-content', true);
+
+		// Plot all of the states
+		const projection = geoAlbersUsa();
+		const path = d3.geoPath().projection(projection);
+		const states = feature(us as any, (us as any).objects.states).features;
+
+		chart
+			.selectAll('path')
+			.data(states)
+			.enter()
+			.append('path')
+			.attr('class', 'states')
+			.attr('d', path);
+	});
+</script>
+
+<div bind:this={el} class="chart"></div>
